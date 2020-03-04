bd3fffbc58ca47ab991aeb36054806611381462d
Mina Malaj <mina.malaj@hotmail.com>
2020-03-04 14:05:51 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,22 @@
-# Add your variables here
+first_number = "" 
+p first_number.to_i
+
+
+
+
+
+=begin 
+def add(first_number,second_number)
+  return first_number + second_number
+end
+def sub (first_number,second_number)
+  return first_number - second_number
+end
+def multiply(first_number,second_number)
+  return first_number * second_number
+end
+def divide(first_number,second_number)
+  return first_number / second_number
+end
+
+=end 
